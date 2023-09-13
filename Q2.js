72a5fd4c8bc7d2263f83d35ed29e91202c9996b9
Aslam Khan shah <aksi34012@gmail.com>
2023-09-13 12:38:54 +0530

    question 2

---
+++ Q2.js
@@ -0,0 +1,17 @@
+//Q.2 ->  Create a Node.js file that appends the existing file with a new content using the File System module.
+
+const fs = require('fs');
+
+const filepath ='demo.txt' ;
+
+const msgtoadd = "this is a text that has been added using fs .  this directoly comes from Q2.js file ......."
+
+
+fs.appendFile(filepath,msgtoadd,(err)=>{
+    if(err) {
+        console.error('yo, try again ! , some unexpected occured')
+    } else {
+        console.log("File updated successfully")
+    }
+    
+});
